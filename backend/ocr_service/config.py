c6c274db96fa3c14bb5c4553c2bb47c604a5d264
Andrea Rossi <andrea@MBP-di-Andrea-3.Home>
2022-01-05 11:06:11 +0100

    tiny augmentation changes + return to OCR Camera configuration

--- backend/ocr_service/config.py
+++ backend/ocr_service/config.py
@@ -3,13 +3,15 @@ import os
 
 OCR_ROOT = os.path.abspath(os.path.join(os.path.abspath(__file__), os.path.pardir))
 STORED_MODELS_PATH = os.path.join(OCR_ROOT, 'stored_models/')
-MODEL_PATH = os.path.join(STORED_MODELS_PATH, 'biagini_model.hdf5')
+# MODEL_PATH = os.path.join(STORED_MODELS_PATH, 'ocr-model.hdf5')
+MODEL_PATH = os.path.join(STORED_MODELS_PATH, 'biagini.hdf5')
+
 data_path = '/Users/andrea/matrices_dataset/'
 
-OCR_INPUT_IMAGE_SHAPE = (640, 64, 1)
-#OCR_INPUT_IMAGE_SHAPE = (1024, 128, 1)
+# OCR_INPUT_IMAGE_SHAPE = (640, 64, 1)
+OCR_INPUT_IMAGE_SHAPE = (1024, 128, 1)
 
-OCR_MAX_TEXT_LENGTH = 180
-#OCR_MAX_TEXT_LENGTH = 128
+# OCR_MAX_TEXT_LENGTH = 180
+OCR_MAX_TEXT_LENGTH = 128
 
 CHARSET_BASE = string.printable[:95]
