a5ba1ceb00776dbc655856bec25b52d08821b526
Andrea Rossi <andrea@MBP-di-Andrea-3.Home>
2022-01-18 16:48:53 +0100

    fixed ocr config.py

--- backend/ocr_service/config.py
+++ backend/ocr_service/config.py
@@ -4,9 +4,9 @@ import os
 OCR_ROOT = os.path.abspath(os.path.join(os.path.abspath(__file__), os.path.pardir))
 STORED_MODELS_PATH = os.path.join(OCR_ROOT, 'stored_models/')
 # MODEL_PATH = os.path.join(STORED_MODELS_PATH, 'ocr-model.hdf5')
-MODEL_PATH = os.path.join(STORED_MODELS_PATH, 'biagini.hdf5')
+MODEL_PATH = os.path.join(STORED_MODELS_PATH, 'biagini_model.hdf5')
 
-data_path = '/Users/andrea/matrices_dataset/'
+data_path = '/path/to/your/matrices/dataset/folder'
 
 # OCR_INPUT_IMAGE_SHAPE = (640, 64, 1)
 OCR_INPUT_IMAGE_SHAPE = (1024, 128, 1)
