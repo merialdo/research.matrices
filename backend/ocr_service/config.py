549ee5db0f023e5ac547dc60b2f1d68238993d40
Andrea Rossi <andrea@MBP-di-Andrea-3.Home>
2022-01-26 21:47:44 +0100

    updated configuration files

--- backend/ocr_service/config.py
+++ backend/ocr_service/config.py
@@ -3,9 +3,7 @@ import os
 
 OCR_ROOT = os.path.abspath(os.path.join(os.path.abspath(__file__), os.path.pardir))
 STORED_MODELS_PATH = os.path.join(OCR_ROOT, 'stored_models/')
-# MODEL_PATH = os.path.join(STORED_MODELS_PATH, 'ocr-model.hdf5')
-MODEL_PATH = os.path.join(STORED_MODELS_PATH, 'biagini_model.hdf5')
-
+MODEL_PATH = os.path.join(STORED_MODELS_PATH, 'biagini_ocr_model.hdf5')
 data_path = '/path/to/your/matrices/dataset/folder'
 
 # OCR_INPUT_IMAGE_SHAPE = (640, 64, 1)
--- backend/text-detector-service/config.py
+++ backend/text-detector-service/config.py
@@ -1,1 +1,4 @@
-MODEL_PATH = 'weights/base_weights.h5'
+#MODEL_PATH = 'weights/base_weights.h5'
+MODEL_PATH = 'weights/biagini_segmentation_model.hdf5'
+
+
